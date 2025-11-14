085f5c3c63993424f7ea52fdc8244e417a0f6dd4
RJ <robojunior894@gmail.com>
2025-11-15 00:03:25 +0300

    changed Identifier.of() to new Identifier() UseKeyS2CPacket & UseKeyC2SPacket changes CustomPayload to FabricPacket changed to packetbytebuff instead of registrybytebuff changed getId() to getType() because 1.21+ i think it uses CustomPayload with PacketCodec changed that
    ModefiteNetworking changes
    removed PayloadTypeRegistry now i use packetypecreate

--- src/main/java/timmychips/modefiteitemdefinitions/ModefiteNetworking.java
+++ src/main/java/timmychips/modefiteitemdefinitions/ModefiteNetworking.java
@@ -1,6 +1,5 @@
 package timmychips.modefiteitemdefinitions;
 
-import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
@@ -9,22 +8,20 @@ import java.util.UUID;
 
 public class ModefiteNetworking {
     public static void registerPayloads() {
-        PayloadTypeRegistry.playC2S().register(UseKeyC2SPayload.PACKET_ID, UseKeyC2SPayload.CODEC);
-        PayloadTypeRegistry.playS2C().register(UseKeyS2CPayload.PACKET_ID, UseKeyS2CPayload.CODEC);
+
     }
 
     public static void useKeyGlobalReceiver() {
-        ServerPlayNetworking.registerGlobalReceiver(UseKeyC2SPayload.PACKET_ID, (payload, context) -> {
-            ServerPlayerEntity sender = context.player();
-            UUID senderUuid = payload.playerUuid();
-            ItemStack stack = payload.itemStack();
-            boolean isUsing = payload.isUsing();
+        ServerPlayNetworking.registerGlobalReceiver(UseKeyC2SPayload.TYPE, (packet, player, responseSender) -> {
+            UUID senderUuid = packet.playerUuid();
+            ItemStack stack = packet.itemStack();
+            boolean isUsing = packet.isUsing();
 
-            UseKeyS2CPayload broadcastPayload = new UseKeyS2CPayload(senderUuid, stack, isUsing);
+            UseKeyC2SPayload broadcastPacket = new UseKeyC2SPayload(senderUuid, stack, isUsing);
 
-            for (ServerPlayerEntity player : sender.server.getPlayerManager().getPlayerList()) {
-                if (!player.getUuid().equals(senderUuid)) {
-                    ServerPlayNetworking.send(player, broadcastPayload);
+            for (ServerPlayerEntity otherPlayer : player.server.getPlayerManager().getPlayerList()) {
+                if (!otherPlayer.getUuid().equals(senderUuid)) {
+                    ServerPlayNetworking.send(otherPlayer, broadcastPacket);
                 }
             }
         });
